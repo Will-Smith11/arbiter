b0217ced7f262a93ccde827c88d3e0d077b6fc47
Colin Roberts <colin@primitive.xyz>
2023-08-11 10:55:22 -0400

    clean

--- arbiter-core/src/bindings/mod.rs
+++ arbiter-core/src/bindings/mod.rs
@@ -4,4 +4,3 @@
 pub mod arbiter_math;
 pub mod arbiter_token;
 pub mod liquid_exchange;
-pub mod counter;
--- arbiter-core/src/tests/strategies.rs
+++ arbiter-core/src/tests/strategies.rs
@@ -1,57 +1,18 @@
 use async_std::channel;
 use ethers_core::types::U256;
 use crossbeam_channel::unbounded;
-use crate::bindings::counter::Counter;
 
 use super::*;
 
-/// This is the arbitraguer strategy.
-pub struct ArbitrageurStrategy {
-    client: Arc<RevmMiddleware>,
-    /// I am not sure the best way to make the contracts generic
-    // What i would like to do is have the contstructor take in two exchange contracts and then we don't neeed the client
-    // There might be a way to just use the client and maybe the exchange addresses, but I am not sure if it will be clean.
-    // exchanges: (LiquidExchange<RevmMiddleware>, <RevmMiddleware>),
-    exchange_prices: (f64, f64),
-    event_sender: crossbeam_channel::Sender<SimulationEvents>,
-}
-
-impl ArbitrageurStrategy {
-    /// Constructor for the [`ArbitraguerStrategy`].
-    pub fn new(
-        client: Arc<RevmMiddleware>,
-        event_sender: crossbeam_channel::Sender<SimulationEvents>,
-    ) -> Self {
-        Self {
-            client,
-            exchange_prices: (0.0, 0.0),
-            event_sender,
-        }
-    }
-
-    /// This function builds two function calls to execute an arbitrage on the liquid exchange and the external market
-    pub fn build_arbitrage_call(
-        &self,
-        _arb_size: usize,
-    ) -> (ContractFunctionCall, ContractFunctionCall) {
-        // one call for each leg
-        todo!()
-    }
-}
-
 pub struct TestStrategy {
     pub name: String,
-    pub counter: Counter<RevmMiddleware>,
-    pub count: usize,
     pub sender: crossbeam_channel::Sender<SimulationEvents>
 }
 
 impl TestStrategy {
-    pub fn new<S: Into<String>>(name: S, counter: Counter<RevmMiddleware>, sender: crossbeam_channel::Sender<SimulationEvents>) -> Self {
+    pub fn new<S: Into<String>>(name: S, sender: crossbeam_channel::Sender<SimulationEvents>) -> Self {
         Self {
             name: name.into(),
-            counter,
-            count: 0,
             sender,
         }
     }
@@ -95,12 +56,12 @@ async fn init() -> Result<()> {
     
     let _ = manager.add_environment(TEST_ENV_LABEL, 1.0, 1, Engine::new());
     let client = Arc::new(RevmMiddleware::new(manager.environments.get(TEST_ENV_LABEL).unwrap()));
-    // make a channel between the collector and the strategy
-    let (send, rec) = crossbeam_channel::unbounded(); 
-    // make strategy, collector, and executor
-    let counter = Counter::new(ethers::types::H160::default(), client.clone());
-    let strategy = TestStrategy::new("test", counter, send.clone());
-    let collector = SimulationCollector::new(rec);
+
+    // make a channel to use for communication of events
+    let (sender, receiver) = crossbeam_channel::unbounded(); 
+    
+    let strategy = TestStrategy::new("test", sender.clone());
+    let collector = SimulationCollector::new(receiver);
     let executor = SimulationExecutor::new(client.clone());
 
     // TODO: Giving the manager a way to add all of these and control the engine would be better.
@@ -108,25 +69,15 @@ async fn init() -> Result<()> {
     manager.environments.get_mut(TEST_ENV_LABEL).unwrap().engine().add_strategy(Box::new(strategy));
     manager.environments.get_mut(TEST_ENV_LABEL).unwrap().engine().add_executor(Box::new(executor));
     
-    let _ = manager.start_environment(TEST_ENV_LABEL).await;
-        // Deploy a counter
-        let counter = Counter::deploy(client.clone(), ())?.send().await?;
-        println!("Counter address: {}", counter.address());
-    send.send(SimulationEvents::Message("hello to the strategy".to_string())).unwrap();
+    manager.start_environment(TEST_ENV_LABEL).await?;
+    sender.send(SimulationEvents::Message("hello to the strategy".to_string())).unwrap();
     
-
     Ok(())
 }
 
 
 #[tokio::test]
 async fn test_strategy() -> Result<()> {
-    // tracing_subscriber::fmt::init();
-    let arbiter_token = init().await?;
-    // println!("{:?}", arbiter_token);
-    // assert_eq!(
-    //     arbiter_token.address(),
-    //     Address::from_str("0x1a9bb958b1ea4d24475aaa545b25fc2e7eb0871c").unwrap()
-    // );
+    init().await?;
     Ok(())
 }
