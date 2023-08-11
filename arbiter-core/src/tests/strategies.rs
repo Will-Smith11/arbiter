8f3cbb84aa0eb446ed334219e928b33518bbf30a
kinrezc <matt.czernik@gmail.com>
2023-08-11 15:50:33 -0400

    fix strategies test

--- arbiter-core/src/tests/strategies.rs
+++ arbiter-core/src/tests/strategies.rs
@@ -1,16 +1,19 @@
 use async_std::channel;
-use ethers_core::types::U256;
 use crossbeam_channel::unbounded;
+use ethers_core::types::U256;
 
 use super::*;
 
 pub struct TestStrategy {
     pub name: String,
-    pub sender: crossbeam_channel::Sender<SimulationEvents>
+    pub sender: crossbeam_channel::Sender<SimulationEvents>,
 }
 
 impl TestStrategy {
-    pub fn new<S: Into<String>>(name: S, sender: crossbeam_channel::Sender<SimulationEvents>) -> Self {
+    pub fn new<S: Into<String>>(
+        name: S,
+        sender: crossbeam_channel::Sender<SimulationEvents>,
+    ) -> Self {
         Self {
             name: name.into(),
             sender,
@@ -22,7 +25,12 @@ impl TestStrategy {
 impl Strategy<SimulationEvents, SimulationActions> for TestStrategy {
     async fn sync_state(&mut self) -> Result<()> {
         println!("syncing state");
-        
+        self.sender
+            .send(SimulationEvents::Message(
+                "hello from the sync state".to_string(),
+            ))
+            .unwrap();
+
         return Ok(());
     }
 
@@ -40,7 +48,6 @@ impl Strategy<SimulationEvents, SimulationActions> for TestStrategy {
     }
 }
 
-
 /// Notes: Currently the deploy strategy works but then breaks when there is no more events comming from the collector.
 /// I do not believe it makes sense to have a strategy for deploying a contract.
 /// My thoughts are to build a more closed system to test this with.
@@ -53,28 +60,51 @@ impl Strategy<SimulationEvents, SimulationActions> for TestStrategy {
 async fn init() -> Result<()> {
     let mut manager = Manager::new();
 
-    
     let _ = manager.add_environment(TEST_ENV_LABEL, 1.0, 1, Engine::new());
-    let client = Arc::new(RevmMiddleware::new(manager.environments.get(TEST_ENV_LABEL).unwrap()));
+    let client = Arc::new(RevmMiddleware::new(
+        manager.environments.get(TEST_ENV_LABEL).unwrap(),
+    ));
 
     // make a channel to use for communication of events
-    let (sender, receiver) = crossbeam_channel::unbounded(); 
-    
+    let (sender, receiver) = crossbeam_channel::unbounded();
+
     let strategy = TestStrategy::new("test", sender.clone());
     let collector = SimulationCollector::new(receiver);
     let executor = SimulationExecutor::new(client.clone());
 
     // TODO: Giving the manager a way to add all of these and control the engine would be better.
-    manager.environments.get_mut(TEST_ENV_LABEL).unwrap().engine().add_collector(Box::new(collector));
-    manager.environments.get_mut(TEST_ENV_LABEL).unwrap().engine().add_strategy(Box::new(strategy));
-    manager.environments.get_mut(TEST_ENV_LABEL).unwrap().engine().add_executor(Box::new(executor));
-    
+    manager
+        .environments
+        .get_mut(TEST_ENV_LABEL)
+        .unwrap()
+        .engine()
+        .add_collector(Box::new(collector));
+    manager
+        .environments
+        .get_mut(TEST_ENV_LABEL)
+        .unwrap()
+        .engine()
+        .add_strategy(Box::new(strategy));
+    manager
+        .environments
+        .get_mut(TEST_ENV_LABEL)
+        .unwrap()
+        .engine()
+        .add_executor(Box::new(executor));
+
+    // 1. start env in initialization
+    // 2. start engine
+    // 3. set is threads that the strategy collectors n executors are in
+    sender
+        .send(SimulationEvents::Message(
+            "hello to the strategy".to_string(),
+        ))
+        .unwrap();
     manager.start_environment(TEST_ENV_LABEL).await?;
-    sender.send(SimulationEvents::Message("hello to the strategy".to_string())).unwrap();
-    
+    println!("Here");
+
     Ok(())
 }
-
 
 #[tokio::test]
 async fn test_strategy() -> Result<()> {
